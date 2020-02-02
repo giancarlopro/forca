bb806fb39d81ac15278c5f3f716ef319aaadb96e
Giancarlo Rocha <giancarloiff@gmail.com>
2020-02-02 20:05:47 -0300

    add: PalavaraRepository interface

---
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraRepository.java
@@ -0,0 +1,21 @@
+package br.edu.iff.bancodepalavras.dominio.palavra;
+
+import java.util.List;
+
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.bancodepalavras.dominio.palavra.Palavra;
+import br.edu.iff.dominio.repository.Repository;
+import br.edu.iff.dominio.repository.RepositoryException;
+
+/**
+ * PalavraRepository
+ */
+public interface PalavraRepository extends Repository {
+    public Palavra getPorId(long id);
+    public List<Palavra> getPorTema(Tema tema);
+    public List<Palavra> getTodas();
+    public Palavra getPalavra(String palavra);
+    public void inserir(Palavra palavra) throws RepositoryException;
+    public void atualizar(Palavra palavra) throws RepositoryException;
+    public void remover(Palavra palavra) throws RepositoryException;
+}
