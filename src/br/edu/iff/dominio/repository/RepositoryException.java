aef7dc7344b71341434446c1e7429e2263f3cc95
Giancarlo Rocha <giancarloiff@gmail.com>
2020-02-02 20:00:20 -0300

    add: repository exception

---
+++ src/br/edu/iff/dominio/repository/RepositoryException.java
@@ -0,0 +1,8 @@
+package br.edu.iff.dominio.repository;
+
+/**
+ * RepositoryException
+ */
+public class RepositoryException extends Exception{
+    private static final long serialVersionUID = 1L;
+}
