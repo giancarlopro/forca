ef4d61d0b154dbf5e56f948b52864d7039fe0ce0
Giancarlo Rocha <giancarloiff@gmail.com>
2020-02-03 13:07:47 -0300

    add: elemento grafico factories

---
+++ src/br/edu/iff/dominio/ElementoGraficoFactory.java
@@ -0,0 +1,9 @@
+package br.edu.iff.dominio;
+
+import br.edu.iff.bancodepalavras.dominio.letra.LetraFactory;
+import br.edu.iff.jogoforca.dominio.boneco.BonecoFactory;
+
+/**
+ * ElementoGraficoFactory
+ */
+public interface ElementoGraficoFactory extends BonecoFactory, LetraFactory{ }
